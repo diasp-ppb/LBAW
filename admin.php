a8422847215207ad620c3d005756cdaf09178cf6
Diogo Campos <d.diogocampos@gmail.com>
2017-03-01 19:11:53 +0000

    Added rough draft for admin page and user list

---
+++ admin.php
@@ -0,0 +1,41 @@
+<?php
+include "templates/header.php";
+include 'templates/userElem.php';
+?>
+    <div style="padding:0; margin: 0 auto; width: 100%" class="container">
+        <div class="row">
+            <div class="col-md-2 col-md-offset-1 additional">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 list-group">
+                        <a href="#user" class="text-center list-group-item active">
+                            <i class="glyphicon glyphicon-user"></i>
+                            User List
+                        </a>
+                        <a href="#stat" class="text-center list-group-item">
+                            <i class="glyphicon glyphicon-dashboard"></i>
+                            Statistics
+                        </a>
+                        <a href="#backup" class="text-center list-group-item">
+                            <i class="glyphicon glyphicon-hdd"></i>
+                            Backup
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8 main">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 list-group">
+                        <?php
+                        userElem('Whiskas Saquetas');
+                        userElem('João Varvosa');
+                        userElem('Pedro Dias que não está preso');
+                        userElem('Pedro Dias que está preso');
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+include "templates/footer.php"
+?>
---
+++ templates/userElem.php
@@ -0,0 +1,15 @@
+<?php function userElem($username) {
+
+    /*
+    if (!defined('BASEPATH'))
+         exit('No direct script access allowed');
+    */
+
+    require_once('smarty/smarty.php');
+
+    $smarty = new MySmarty;
+
+    $smarty->assign('username', $username);
+
+    $smarty->display('userElem.tpl');
+} ?>

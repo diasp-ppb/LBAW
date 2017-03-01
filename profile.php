626e943192efc4a43c53141ddf91394a708bf746
João Barbosa <bmpj13@hotmail.com>
2017-03-01 18:46:20 +0000

    Profile almost done!

--- profile.php
+++ profile.php
@@ -15,7 +15,7 @@ include "templates/header.php"
                 <div class="text-center">
                     Estudante
                 </div>
-                <div class="list-group">
+                <div class="col-md-12 list-group">
                     <a href="#" class="list-group-item active">
                         <i class="glyphicon glyphicon-home"></i>
                         Profile
@@ -28,34 +28,76 @@ include "templates/header.php"
             </div>
         </div>
         <div class="col-md-8 col-md-offset-1 main">
-            <div style="margin: 0 auto; width: 100%" class="container">
-                <div class="row">
-                    <div class="col-md-2">
-                        <div class="upvote attach">
-                            ^
-                        </div>
-                        <div class="rating attach">
-                            0
-                        </div>
-                        <div class="downvote attach">
-                            v
+            <div class="row">
+                <div class="col-md-10">
+                    <div class="row section">
+                        <h3>Atividade</h3>
+                        <div class="timeline-centered">
+                            <article class="timeline-entry">
+
+                                <div class="timeline-entry-inner">
+
+                                    <div class="timeline-icon bg-primary">
+                                        <i class="entypo-feather"></i>
+                                    </div>
+
+                                    <div class="timeline-label">
+                                        <h2><a href="#">Varvosinha</a> <span>aceitou a tua resposta.</span></h2>
+                                        <p>A tua resposta ao tópico "Porque é que sou giro?" satisfez os requisitos do utilizador com sucesso.</p>
+                                    </div>
+                                </div>
+
+                            </article>
                         </div>
                     </div>
-                    <div class="col-md-10">
+                    <div class="row section">
+                        <h3>As minhas estatísticas</h3>
+                        <div class="col col-md-5">
+                            <h4>Respostas aceites:</h4>
+                            <span class="tag">Java</span>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" role="progressbar" style="width:100%">5</div>
+                            </div>
+
+                            <span class="tag">Python</span>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" role="progressbar" style="width:60%">3</div>
+                            </div>
+
+                            <span class="tag">Go</span>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" role="progressbar" style="width:20%">1</div>
+                            </div>
+                        </div>
+                        <div class="col col-md-5">
+                            <h4>Onde comento:</h4>
+                            <span class="tag">Java</span>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" role="progressbar" style="width:100%">100</div>
+                            </div>
+
+                            <span class="tag">Go</span>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" role="progressbar" style="width:68%">68</div>
+                            </div>
+
+                            <span class="tag">C++</span>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success" role="progressbar" style="width:34%">34</div>
+                            </div>
+
+                            <span class="tag">Scheme</span>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-warning" role="progressbar" style="width:3%">1</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row section">
+                        <h3>Os meus tópicos</h3>
                         <a class="title" href="#">Porque é que o Diogo é bicha e feio, mas mesmo assim gostava de o afiambrar? Não entendo esta minha vida meu deus meu deus nossa senhora</a>
                         <div class="tags">
                             <span class="tag">Java</span> <span class="tag">Homossexualidade</span>
                         </div>
-                    </div>cd <Doc></Doc>
-                </div>
-                <div class="row">
-                    <div class="col-md-10 pull-right">
-                        Porque é que a amizade é tão gira?
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-10 pull-right">
-                        <p>Postado por: João Varvosa</p>
                     </div>
                 </div>
             </div>
--- topic.php
+++ topic.php
@@ -6,7 +6,7 @@ include "templates/header.php"
 <div style="padding:0; margin: 0 auto; width: 100%" class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2 main">
-            <div class="row">
+            <div class="row attach-container">
                 <div class="col-md-1 col-md-offset-1 attach">
                     <div class="row upvote">
                         ^
@@ -19,22 +19,26 @@ include "templates/header.php"
                     </div>
                 </div>
                 <div class="col-md-10 topic-main">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a class="title" href="#">Porque é que o Diogo é bicha e feio, mas mesmo assim gostava de o afiambrar? Não entendo esta minha vida meu deus meu deus nossa senhora</a>
-                            <div class="tags">
-                                <span class="tag">Java</span> <span class="tag">Homossexualidade</span>
-                            </div>
+                    <div class="row topic-header">
+                        <a class="title" href="#">Porque é que o Diogo é bicha e feio, mas mesmo assim gostava de o afiambrar? Não entendo esta minha vida meu deus meu deus nossa senhora</a>
+                        <div class="tags">
+                            <span class="tag">Java</span> <span class="tag">Homossexualidade</span>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            Porque é que a amizade é tão gira?
+                    <div class="row topic-body">
+                        Porque é que a amizade é tão gira?
+                    </div>
+                    <div class="row topic-footer">
+                        <div class="author">
+                            João Varvosa
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <p>Postado por: João Varvosa</p>
+                    <div class="row comment">
+                        <div class="comment-body">
+                            Eu acho que és parvo, só isso.
+                        </div>
+                        <div class="author">
+                            Rodrigo Jorge
                         </div>
                     </div>
                 </div>

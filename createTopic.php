c1453d595c8735244ca6757c97ed50292b4145ee
rodavoce <up201404178@fe.up.pt>
2017-03-02 09:16:36 +0000

    CreateTopi styled

--- createTopic.php
+++ createTopic.php
@@ -3,29 +3,34 @@ include "templates/header.php"
 
 
 ?>
+<div class="col-md-2" style="background-color:#dee2e8; height:100%;">
 
-<div class="col-md-offset-3 col-md-6 form text-center">
+
+</div>
+
+
+<div class="col-md-offset-1 col-md-6 form text-center">
     <div id="row text-center">
         <h2> Criar Tópico </h2>
     </div>
-    <div class="row form-group">
+    <div class="form-group">
              <label for="title">Titulo</label>
-             <input type="text" class="form-control" id="title" />
+             <input type="text" class="form-control" id="title" placeholder="Titulo da pergunta" />
     </div>
 
         </br>
   
         
-    <div class="row form-group">
+    <div class=" form-group">
              <label for="tags">Tags</label>
-             <input type="text" class="form-control" id="tags" />
+             <input type="text" class="form-control" id="tags" placeholder="Tags"/>
     </div>
         
     </br>
 
 
-    <div class="row form-group">
-             <textarea class="form-control" id="content" style="min-height: 9% " value=""></textarea>
+    <div class=" form-group">
+             <textarea class="form-control" id="content" style="min-height: 9% " value="" placeholder="Pergunta aqui "></textarea>
     </div>
 
     
@@ -38,4 +43,12 @@ include "templates/header.php"
 
  </div>
 
+ <div class="col-md-2 col-md-offset-1" style="background-color:#dee2e8; height:100%;">
+
+
+</div>
+
+
 <div>
+
+<?php include  "templates/footer.php"?>

c321f701f81a773736630dfae7bafe13bb6919d4
João Barbosa <bmpj13@hotmail.com>
2017-03-02 21:30:39 +0000

    statistics on admin page

--- admin.php
+++ admin.php
@@ -29,7 +29,7 @@ include 'templates/userElem.php';
                     <div class="col-md-10 col-md-offset-1">
                         <section id="userlist">
                             <div class="page-header">
-                                <h1>User List</h1>
+                                <h1>Lista de Utilizadores</h1>
                             </div>
                             <div class="list-group">
                                 <?php
@@ -46,16 +46,72 @@ include 'templates/userElem.php';
                     <div class="col-md-10 col-md-offset-1">
                         <section id="statistics">
                             <div class="page-header">
-                                <h1>Statistics</h1>
+                                <h1>Estatísticas</h1>
                             </div>
-                        </section>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-10 col-md-offset-1">
-                        <section id="backup">
-                            <div class="page-header">
-                                <h1>Backups</h1>
+                            <div class="row">
+                                <div class="col-md-2 col-md-offset-1 statistic">
+                                    <h4>Utilizadores</h4>
+                                    <span class="glyphicon glyphicon-user blue"></span>
+                                    <div class="statistic amount">12455</div>
+                                </div>
+                                <div class="col-md-2 col-md-offset-2 statistic">
+                                    <h4>Utilizadores ativos</h4>
+                                    <span class="glyphicon glyphicon-user green"></span>
+                                    <div class="statistic amount">12455</div>
+                                </div>
+                                <div class="col-md-2 col-md-offset-2 statistic">
+                                    <h4>Tópicos criados</h4>
+                                    <span class="glyphicon glyphicon-question-sign orange"></span>
+                                    <div class="statistic amount">12455</div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <canvas id="topics-by-course"></canvas>
+                                <script>
+                                    var ctx = document.getElementById("topics-by-course").getContext('2d');
+                                    var myChart = new Chart(ctx, {
+                                        type: 'doughnut',
+                                        data: {
+                                            labels: ["M", "T", "W", "T", "F", "S", "S"],
+                                            datasets: [{
+                                                backgroundColor: [
+                                                    "#2ecc71",
+                                                    "#3498db",
+                                                    "#95a5a6",
+                                                    "#9b59b6",
+                                                    "#f1c40f",
+                                                    "#e74c3c",
+                                                    "#34495e"
+                                                ],
+                                                data: [12, 19, 3, 17, 28, 24, 7]
+                                            }]
+                                        }
+                                    });
+                                </script>
+                            </div>
+                            <div class="row">
+                                <canvas id="topics-by-tags"></canvas>
+                                <script>
+                                    var ctx = document.getElementById("topics-by-tags").getContext('2d');
+                                    var myChart = new Chart(ctx, {
+                                        type: 'bar',
+                                        data: {
+                                            labels: ["M", "T", "W", "T", "F", "S", "S"],
+                                            datasets: [{
+                                                backgroundColor: [
+                                                    "#2ecc71",
+                                                    "#3498db",
+                                                    "#95a5a6",
+                                                    "#9b59b6",
+                                                    "#f1c40f",
+                                                    "#e74c3c",
+                                                    "#34495e"
+                                                ],
+                                                data: [12, 19, 3, 17, 28, 24, 7]
+                                            }]
+                                        }
+                                    });
+                                </script>
                             </div>
                         </section>
                     </div>

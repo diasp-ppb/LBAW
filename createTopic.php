7590422e8849916650b5120e6ac4e2fddb7142a8
José Pedro Teixeira Monteiro <j.pedroteixeira.monteiro@gmail.com>
2017-03-05 22:20:30 +0000

    some changes and corrections in createTopic

--- createTopic.php
+++ createTopic.php
@@ -6,39 +6,27 @@ include "templates/header.php"
   <script src="js/marked.min.js"></script>
   <link rel="stylesheet" href="css/bootstrap-markdown.min.css">
 
-
-
-  <div class="col-md-offset-2 col-md-8 form text-center">
-    <div id="row text-center">
-      <h2>Criar Tópico</h2>
-    </div>
-    <div class="form-group">
-      <label for="title">Titulo</label>
-      <input type="text" class="form-control" id="title" placeholder="Titulo da pergunta" />
-    </div>
-
-
-
-
-    <div class=" form-group">
-      <label for="tags">Tags</label>
-      <input type="text" class="form-control" id="tags" placeholder="Tags" />
-    </div>
-
-
-
-    <div class=" form-group">
-      <textarea name="content" data-provide="markdown" rows="10"></textarea>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8 form text-center">
+                <div id="row text-center">
+                    <label id="top-title">Criar Tópico</label>
+                </div>
+                <div class="form-group">
+                    <label for="title">Titulo</label>
+                    <input type="text" class="form-control" id="title" placeholder="Titulo da pergunta"/>
+                </div>
+                <div class=" form-group">
+                    <label for="tags">Tags</label>
+                    <input type="text" class="form-control" id="tags" placeholder="Tags"/>
+                </div>
+                <div class=" form-group">
+                    <textarea name="content" data-provide="markdown" rows="10"></textarea>
+                </div>
+                <input type="submit" class="btn btn-info" value="Submit Button"/>
+            </div>
+        </div>
     </div>
 
-    <input type="submit" class="btn btn-info" value="Submit Button" />
-    </br>
 
-  </div>
-
-
-
-
-  <div>
-
-    <?php include  "templates/footer.php"?>
+<?php include  "templates/footer.php"?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,12 +1,15 @@
 
-        <div class="container">
-            <hr>
-            <div class="text-center center-block">
-                <p class="txt-railway">Diogo Campos - João Barbosa - José Pedro Monteiro - Pedro Dias</p>
-                <a href=""><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
-                <a href=""><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
-                <a href=""><i id="social-gh" class="fa fa-github fa-3x social"></i></a>
-            </div>
+<footer class="footer">
+    <div class="container">
+        <hr>
+        <div class="text-center center-block">
+            <p class="txt-railway">Diogo Campos - João Barbosa - José Pedro Monteiro - Pedro Dias</p>
+            <a href=""><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+            <a href=""><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+            <a href=""><i id="social-gh" class="fa fa-github fa-3x social"></i></a>
         </div>
+    </div>
+</footer>
+
     </body>
 </html>

74c74cee908e4af8b45bcc854af7b338689145f7
José Pedro Teixeira Monteiro <j.pedroteixeira.monteiro@gmail.com>
2017-03-05 21:23:31 +0000

    error page and footer completed

--- error.php
+++ error.php
@@ -2,17 +2,40 @@
 include "templates/header.php"
 ?>
 
-<div class="container-fluid">
+<div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2 main">
-            <div class="row">
-                <div class="alert alert-danger">
-                    <strong>DEU MERDA!</strong>
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="header hidden-xs hidden-sm">
+                <div id="logo"></div>
+            </div>
+        </div>
+        <div class="col-md-2">
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <h3 class="text-center">
+                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> Oops:
+                        <small>Page not found - <b>404 error</b></small>
+                    </h3>
                 </div>
-                <div class="alert alert-warning">
-                    Volte atrás chachabor
+                <div class="panel-body">
+                    <p> A página que procuras não se encontra disponível. Verifique as seguintes possíveis soluções:</p>
+                    <ul class="list-group">
+                        <li class="list-group-item">Garanta que o <em>link</em> utilizado está correto.</li>
+                        <li class="list-group-item">Se chegou a esta página carregando num <em>link</em>, contacte-nos para reportar o erro.</li>
+                        <li class="list-group-item">Esqueça que isto acontence e volte para a página anterior :)</li>
+                    </ul>
                 </div>
             </div>
+        </div>
+        <div class="col-md-2">
+
         </div>
     </div>
 </div>

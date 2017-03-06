e35457cf4c697c948ba3c1d05998d8558ce8b01d
João Barbosa <bmpj13@hotmail.com>
2017-03-06 18:12:20 +0000

    fixed topic page

--- topic.php
+++ topic.php
@@ -3,72 +3,57 @@ include "templates/header.php"
 ?>
 
 
-<div style="padding:0; margin-top: 6em; width: 100%" class="container">
+<div class="container-fluid">
     <div class="row">
         <div class="col-md-8 col-md-offset-2 main">
             <div class="row attach-container">
-                <div class="col-md-1  attach">
-                    <div class="row upvote btn btn-info">
-                        ^
-                    </div>
-                    <div class="row rating btn btn-info">
-                        0
-                    </div>
-                    <div class="row downvote btn btn-info">
-                        v
-                    </div>
-                </div>
-
                 <div class="col-md-1">
-                    <div class="thumbnail">
-                        <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
+                    <div class="row">
+                        <div class="thumbnail hidden-xs hidden-sm">
+                            <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
+                        </div>
                     </div>
                 </div>
-
-
-                <div class="col-md-10  topic-main  panel panel-default">
+                <div class="col-md-10 topic-main panel panel-default">
                     <div class="row topic-header panel-heading">
-
-                        <div >
+                        <div>
                             <strong>Varvosa</strong> <span class="text-muted">posted 5 days ago</span>
+                            <button type="button" class="upvote btn btn-primary">25</button>
+                            <span class="pull-right">
+                                <button type="button" class="upvote btn btn-danger"><i class="glyphicon glyphicon-circle-arrow-up"></i></button>
+                                <button type="button" class="downvote btn btn-warning btn-circle"><i class="glyphicon glyphicon-circle-arrow-down"></i></button>
+                            </span>
                         </div>
 
-                        <a class="title" href="#" style="overflow-wrap:break-word;">Porque é que o Diogo é bicha e feio, mas mesmo assim gostava de o afiambrar? Não entendo esta minha vida meu deus meu deus nossa senhora</a>
-                        
-                        
-                   
+                        <a class="title" href="#">Porque é que o Diogo é bicha e feio, mas mesmo assim gostava de o afiambrar? Não entendo esta minha vida meu deus meu deus nossa senhora</a>
+
                         <div class="tags">
                             <span class="tag">Java</span> <span class="tag">Homossexualidade</span>
                         </div>
                     </div>
-                    <div class="row topic-body panel-body" style="overflow-wrap:break-word;">
+                    <div class="row topic-body panel-body">
                         Porque é que a amizade é tão gira?dfsfashdfuahsudfadfahuuuuuuuuuuuuuuuuuuuuuuuuuuusdilfhasifhsfhsadfsflsfuslfsfhsdfluasdfhasldfhlsdfsdfsajdfjasdjfkhalsdfhasdfjasdlfladfhadsfkashfdhasdjfjkasdfhalsfdhalsdfhasdf
                     </div>
                 </div>
             </div>
-            <div class="row attach-container">
-                <div class="col-md-1  attach">
-                    <div class="row upvote btn btn-info">
-                        ^
-                    </div>
-                    <div class="row rating btn btn-info">
-                        0
-                    </div>
-                    <div class="row downvote btn btn-info">
-                        v
-                    </div>
-                </div>
 
+            <div class="row attach-container">
                 <div class="col-md-1">
-                    <div class="thumbnail">
+                    <div class="thumbnail hidden-xs hidden-sm">
                         <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
                     </div>
                 </div>
                 <div class="col-md-10 panel panel-default">
-                    <div class="panel-heading">
+                    <div class="comment-header panel-heading">
                         <strong>Varvosa</strong> <span class="text-muted">commented 5 days ago</span>
+                        <button type="button" class="upvote btn btn-primary">-3</button>
+                        <span class="pull-right">
+                            <button type="button" class="upvote btn btn-danger"><i class="glyphicon glyphicon-circle-arrow-up"></i></button>
+                            <button type="button" class="downvote btn btn-warning btn-circle"><i class="glyphicon glyphicon-circle-arrow-down"></i></button>
+                            <button type="button" class="correct btn btn-info btn-circle"><i class="glyphicon glyphicon-ok"></i></button>
+                        </span>
                     </div>
-                    <div class="panel-body" style="overflow-wrap:break-word;">
+                    <div class="comment-body panel-body">
                         Porque é que a amizade é tão gira?
                         dfsfashdfuahsudfadfahuuuuuuuuuuuuuuuuuuuuuuuuuuusdilfhasifhsfhsadfsflsfuslfsfhsdfluasdfhasldfhlsdfsdfsajdfjasdjfkhalsdfhasdfjasdlfladfhadsfkashfdhasdjfjkasdfhalsfdhalsdfhasdf
                         dfsfashdfuahsudfadfahuuuuuuuuuuuuuuuuuuuuuuuuuuusdilfhasifhsfhsadfsflsfuslfsfhsdfluasdfhasldfhlsdfsdfsajdfjasdjfkhalsdfhasdfjasdlfladfhadsfkashfdhasdjfjkasdfhalsfdhalsdfhasdf
@@ -76,13 +61,12 @@ include "templates/header.php"
                         dfsfashdfuahsudfadfahuuuuuuuuuuuuuuuuuuuuuuuuuuusdilfhasifhsfhsadfsflsfuslfsfhsdfluasdfhasldfhlsdfsdfsajdfjasdjfkhalsdfhasdfjasdlfladfhadsfkashfdhasdjfjkasdfhalsfdhalsdfhasdf
                         dfsfashdfuahsudfadfahuuuuuuuuuuuuuuuuuuuuuuuuuuusdilfhasifhsfhsadfsflsfuslfsfhsdfluasdfhasldfhlsdfsdfsajdfjasdjfkhalsdfhasdfjasdlfladfhadsfkashfdhasdjfjkasdfhalsfdhalsdfhasdf
                         dfsfashdfuahsudfadfahuuuuuuuuuuuuuuuuuuuuuuuuuuusdilfhasifhsfhsadfsflsfuslfsfhsdfluasdfhasldfhlsdfsdfsajdfjasdjfkhalsdfhasdfjasdlfladfhadsfkashfdhasdjfjkasdfhalsfdhalsdfhasdf
-
                     </div>
                 </div>
             </div>
-                <div class="col-md-2 col-md-offset-8">
-                    <input type="submit" class="btn btn-info" value="Comentar" />
-                </div>
+            <div class="col-md-2 col-md-offset-10">
+                <input type="submit" class="btn btn-info" value="Comentar" />
+            </div>
         </div>
     </div>
 </div>

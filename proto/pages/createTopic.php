39921af31131d56895fa3d80cf992d3e986d0392
rodavoce <up201404178@fe.up.pt>
2017-04-14 19:25:14 +0100

    extracted textEditor script

--- proto/pages/createTopic.php
+++ proto/pages/createTopic.php
@@ -20,22 +20,8 @@ include "../templates/header.php"
         <div id="wmd-button-bar"></div>
         <textarea id="wmd-input" class="wmd-input"></textarea>
         <div id="wmd-preview" class="wmd-preview text-left well"></div>
-        <script>
-            var converter = Markdown.getSanitizingConverter();
-            var editor = new Markdown.Editor(converter);
-            editor.run();
-        </script>
-
-        <script>
-            $("#wmd-redo-button").addClass( "hidden-xs" );
-            $("#wmd-undo-button").addClass( "hidden-xs" );
-            $("#wmd-spacer3").addClass( "hidden-xs" );
-            $("#wmd-button-row").append("<li class='wmd-spacer hidden-xs' style='left:370'></li>");
-            $("#wmd-button-row").append("<li class='wmd-button hidden-xs' id='help-button'  title='Heading <h1>/<h2> Ctrl+Shift+H' style='left: 385px;'><span style='background-position: -240px 0px;'></span></li>");
-            $("#help-button").click(function(){
-              $("#squarespaceModal").modal('toggle');
-            });     
-        </script>
+        
+        <script type="text/javascript" src="../js/textEditor.js"></script>
 
         <div class="center">
           <input type="submit" class="btn btn-info" value="Submit"/>

233eefc698a74b3c5978861a3561c92da9a7fafe
Diogo Campos <d.diogocampos@gmail.com>
2017-04-20 09:22:07 +0100

    Added error redirects to profile page

--- proto/pages/error.php
+++ proto/pages/error.php
@@ -1,36 +1,7 @@
 <?php
-include "../templates/header.php"
-?>
+include_once("../templates/header.php");
 
-<div class="container">
-    <div class="row">
-        <div class="col-xs-10 col-xs-offset-1 col-md-10 col-md-offset-1">
-            <div class="panel panel-danger">
-                <div class="panel-heading">
-                    <h3 class="text-center">
-                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> Oops:
-                        <small>Page not found - <b>404 ERROR</b></small>
-                    </h3>
-                </div>
-                <div class="panel-body">
-                    <p> A página que procuras não se encontra disponível. Verifique as seguintes possíveis soluções:</p>
-                    <ul class="list-group">
-                        <li class="list-group-item">Garanta que o <em>link</em> utilizado está correto.</li>
-                        <li class="list-group-item">Se chegou a esta página carregando num <em>link</em>, contacte-nos para reportar o erro.</li>
-                        <li class="list-group-item">Esqueça que isto acontece e volte para a página anterior :)</li>
-                    </ul>
+$smarty->display('error.tpl');
 
-                    <div class="row hidden-xs hidden-sm">
-                        <div class="col-xs-4 col-xs-offset-4">
-                            <img alt="Brand" src="resources/images/logo_askabit.png" width="240" height="240">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<?php
-include "../templates/footer.php"
+include_once("../templates/footer.php");
 ?>
--- proto/pages/profile.php
+++ proto/pages/profile.php
@@ -1,11 +1,22 @@
 <?php
+if (!isset($_GET["id"]) || !is_integer($_GET["id"])) {
+    header("Location: ../pages/error.php");
+}
+
+include_once('../config/init.php');
 include_once("../database/account.php");
 include_once("../database/email.php");
 include_once("../database/topics.php");
-include_once("../templates/header.php");
 
 $userid = $_GET["id"];
 $user = getAccountByUserId($userid)[0];
+
+if (!isset($user)) {
+    header("Location: ../pages/error.php");
+}
+
+include_once("../templates/header.php");
+
 $emails = getUserEmailList($userid);
 $links = json_decode(getUserLinks($userid)[0]['links']);
 $topics = getTopicsByUser($userid);

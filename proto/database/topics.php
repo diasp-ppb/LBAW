08bfc913ebc49ebce946fb8edc3cfabf948dfe36
rodavoce <up201404178@fe.up.pt>
2017-04-25 22:12:19 +0100

    home topics fix

--- proto/database/topics.php
+++ proto/database/topics.php
@@ -23,11 +23,11 @@ function getFeaturedTopics() {
     //TODO CRIAR UMA VIEW?!?
     global $conn;
     $stmt = $conn->prepare("SELECT * FROM post post1
-                                JOIN (SELECT post2_1.parentid, COUNT(*) AS COUNT
+                                LEFT JOIN (SELECT post2_1.parentid, COUNT(*) AS COUNT
                                 FROM post post2_1 WHERE post2_1.posttype = 'answer'
                                 GROUP BY post2_1.parentid) post2 ON post1.id = post2.parentid
                             WHERE post1.posttype = 'question'
-                            ORDER BY post1.rating DESC, post2.count LIMIT 10;");
+                            ORDER BY post1.rating DESC,post2.count LIMIT 10;");
     $stmt->execute();
     return $stmt->fetchAll();
 }

70d883f6af66f0121d3e7feca93e30079603aa55
José Pedro Teixeira Monteiro <j.pedroteixeira.monteiro@gmail.com>
2017-05-26 18:34:53 +0100

    images on master done. need github images and redimension

--- proto/actions/member/editProfile.php
+++ proto/actions/member/editProfile.php
@@ -33,4 +33,28 @@ if(isset($_POST['name'])) {
 
 	header("Location: ../../pages/member/profile.php?id=" . $id);
 }
+
+/*if(isset($_POST["git_username"])){
+	$imagePath = "github.com/" . $_POST["git_username"]. ".png";
+	setUserImage($imagePath,$_SESSION["id"]);
+
+	header("Location: ../../pages/member/profile.php?id=" . $_SESSION["id"]);
+} else*/
+if($_FILES["upload-image"]["error"]==0){
+	print_r($_FILES);
+	$destination_directory = "../../resources/images/users/";
+	$temporary = explode(".", $_FILES["upload-image"]["name"]);
+	$file_extension = end($temporary);
+
+	$sourcePath = $_FILES["upload-image"]["tmp_name"];
+	$targetPath = $destination_directory . $_SESSION["username"] . "." . $file_extension;
+
+	$currentImage=getUserImage($_SESSION["id"]);
+	unlink($currentImage);
+	move_uploaded_file($sourcePath, $targetPath);
+	setUserImage($targetPath,$_SESSION["id"]);
+
+	header("Location: ../../pages/member/profile.php?id=" . $_SESSION["id"]);
+}
+
 ?>
---
+++ proto/js/editProfile.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    var maxsize = 500 * 1024; // 500 KB
+    console.log("OLA");
+
+    $('#upload-check').click(function(){
+        $('#git_username').val("");
+        $('#git_username').prop('disabled',true);
+        $('#upload-image').prop('disabled',false);
+        $('#submit-image').prop('disabled',true);
+    });
+
+    $('#github-check').click(function () {
+        $('#upload-image').val("");
+        $('#upload-image').prop('disabled',true);
+        $('#git_username').prop('disabled',false);
+        $('#submit-image').prop('disabled',true);
+    });
+
+    $('#git_username').on('focusout',function () {
+        if($(this).val().length>0){
+            $('#submit-image').removeAttr("disabled");
+        }
+    });
+
+    $('#upload-image').change(function() {
+
+        $('#message').empty();
+
+        var file = this.files[0];
+        var match = ["image/jpeg", "image/png", "image/jpg"];
+
+        if (!((file.type == match[0]) || (file.type == match[1]) || (file.type == match[2])))
+        {
+            noPreview();
+            $('#message').html('<div class="alert alert-warning" role="alert">Formato inválido. São permitidos apenas os formatos seguintes: JPG, JPEG, PNG.</div>');
+            return false;
+        }
+
+        if (file.size > maxsize || file.size==0)
+        {
+            noPreview();
+            $('#message').html('<div class=\"alert alert-danger\" role=\"alert\">O tamanho máximo da imagem para fazer o upload é ' + (maxsize/1024).toFixed(2) + ' KB</div>');
+            return false;
+        }
+
+        $('#submit-image').removeAttr("disabled");
+
+        var reader = new FileReader();
+        reader.onload = selectImage;
+        reader.readAsDataURL(this.files[0]);
+    });
+});
+
+function noPreview() {
+    $('#image-preview').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Turkish_Van_Cat.jpg/700px-Turkish_Van_Cat.jpg');
+    $('#submit-image').prop('disabled',true);
+}
+
+function selectImage(e) {
+    $('#image-preview').attr('src', e.target.result);
+}
